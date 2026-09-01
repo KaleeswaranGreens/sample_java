85c29ca27a3ff5a001025328d4930c99c0bbad8b
KaleeswaranKaran <kaleeswarankaran.08@gmail.com>
2026-09-01 18:21:29 +0530

    Create New Method

--- Sample.java
+++ Sample.java
@@ -1,8 +1,13 @@
+
 public class Sample {
 	
 	public static void main(String[] args) {
 	  		System.out.println("Employee Name is : Karan");
 
+	}
+
+	public void employeeId() {
+		System.out.println("Employee ID is : 12345");
 	}
 
 }
